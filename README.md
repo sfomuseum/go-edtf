8860150691b26131ece6b94b8cd60870f281c4ec
thisisaaronland <devnull@localhost>
2019-01-10 12:27:36 -0800

    stubs and notes to self

--- README.md
+++ README.md
@@ -9,4 +9,5 @@ This is a work in progress and does not work yet. Help is welcome :D
 ## See also
 
 * http://www.loc.gov/standards/datetime/
+* https://www.loc.gov/standards/datetime/pre-submission.html#bnf
 * https://github.com/ixc/python-edtf
--- edtf.go
+++ edtf.go
@@ -1,1 +1,16 @@
 package edtf
+
+import (
+	"errors"
+)
+
+type Date struct {
+
+}
+
+// https://www.loc.gov/standards/datetime/pre-submission.html#bnf
+
+func Parse(edtf_str string) (*Date, error) {
+
+	return nil, errors.New("Please write me")
+}
