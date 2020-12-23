a6e322dbd551d971b2fac89e512723cf3ca705bd
thisisaaronland <devnull@localhost>
2020-12-23 14:58:42 -0800

    add IsLevel0 and ParseLevel0

--- parse.go
+++ parse.go
@@ -1,6 +1,17 @@
 package edtf
 
+import (
+	"errors"
+	"github.com/whosonfirst/go-edtf/level0"
+	_ "github.com/whosonfirst/go-edtf/level1"
+	_ "github.com/whosonfirst/go-edtf/level2"	
+)
+
 func ParseString(edtf_str string) (*EDTFDate, error) {
 
-	return nil, nil
+	if level0.IsLevel0(edtf_str){
+		return level0.ParseLevel0(edtf_str)
+	}
+
+	return nil, errors.New("Invalid or unsupported EDTF string")
 }
