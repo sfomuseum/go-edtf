6e650f9e67bf97864692cf28d376a4a7ed73f443
thisisaaronland <devnull@localhost>
2021-01-06 16:56:09 -0800

    pass all tests again!

--- calendar/calendar_test.go
+++ calendar/calendar_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestDaysInMonth(t *testing.T) {
 
-	tests := map[string]uint{
+	tests := map[string]int{
 		"2019-02": 28,
 		"2020-02": 29,
 		"1985-06": 30,
@@ -15,14 +15,20 @@ func TestDaysInMonth(t *testing.T) {
 
 	for str_ym, expected_days := range tests {
 
+		t.Logf("DaysInMonthWithString '%s'", str_ym)
+			
 		days, err := DaysInMonthWithString(str_ym)
 
 		if err != nil {
-			t.Fatal(err)
+			t.Logf("Failed to determine days in month for '%s', %v", str_ym, err)
+			t.Fail()
+			continue
 		}
 
 		if days != expected_days {
-			t.Fatalf("Failed to determined days in month for '%s', expected '%d' but got '%d'", str_ym, expected_days, days)
+			t.Logf("Failed to determined days in month for '%s', expected '%d' but got '%d'", str_ym, expected_days, days)
+			t.Fail()
+			continue
 		}
 	}
 }
