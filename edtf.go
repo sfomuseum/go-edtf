a427b0dbe3574bc1f8efce6bf5765773c5b45dea
thisisaaronland <devnull@localhost>
2021-01-08 13:31:20 -0800

    add basic parse/unmarshal test; move high level definitions in to their own files

--- edtf.go
+++ edtf.go
@@ -1,9 +1,6 @@
 package edtf
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
 	"time"
 )
 
@@ -16,19 +13,6 @@ const NEGATIVE string = "-"
 
 const HMS_LOWER string = "00:00:00"
 const HMS_UPPER string = "23:59:59"
-
-const (
-	NONE      Precision = 0
-	ALL       Precision = 1 << iota // 2
-	ANY                             // 4
-	DAY                             // 8
-	WEEK                            // 16
-	MONTH                           // 32
-	YEAR                            // 64
-	DECADE                          // 128
-	CENTURY                         // 256
-	MILLENIUM                       // 512
-)
 
 const MAX_YEARS int = 9999 // This is a Golang thing
 
@@ -45,7 +29,7 @@ func (d *EDTFDate) Lower() *time.Time {
 	ts := d.Start.Lower.Timestamp
 
 	if ts == nil {
-
+		return nil
 	}
 
 	return ts.Time()
@@ -62,74 +46,6 @@ func (d *EDTFDate) Upper() *time.Time {
 	return ts.Time()
 }
 
-type DateSpan struct {
-	Start *DateRange `json:"start"`
-	End   *DateRange `json:"end"`
-}
-
-func (s *DateSpan) String() string {
-	return fmt.Sprintf("[[%T] Start: '%v' End: '%v']", s, s.Start, s.End)
-}
-
-type DateRange struct {
-	EDTF  string `json:"edtf"`
-	Lower *Date  `json:"lower"`
-	Upper *Date  `json:"upper"`
-}
-
-func (r *DateRange) String() string {
-	return fmt.Sprintf("[[%T] Lower: '%v' Upper: '%v'[", r, r.Lower, r.Upper)
-}
-
-type Date struct {
-	DateTime    string     `json:"datetime,omitempty"`
-	Timestamp   *Timestamp `json:"timestamp,omitempty"`
-	YMD         *YMD       `json:"ymd"`
-	Uncertain   Precision  `json:"uncertain,omitempty"`
-	Approximate Precision  `json:"approximate,omitempty"`
-	Unspecified Precision  `json:"unspecified,omitempty"`
-	Precision   Precision  `json:"precision,omitempty"`
-	Open        bool       `json:"open,omitempty"`
-	Unknown     bool       `json:"unknown,omitempty"`
-	Inclusivity Precision  `json:"inclusivity,omitempty"`
-}
-
-func (d *Date) SetTime(t *time.Time) {
-	d.DateTime = t.Format(time.RFC3339)
-	d.Timestamp = NewTimestampWithTime(t)
-}
-
-func (d *Date) String() string {
-	return fmt.Sprintf("[[%T] Time: '%v' YMD: '%v']", d, d.Timestamp, d.YMD)
-}
-
-type YMD struct {
-	Year  int `json:"year"`
-	Month int `json:"month"`
-	Day   int `json:"day"`
-}
-
-func (ymd *YMD) String() string {
-	return fmt.Sprintf("[%T] Y: '%d' M: '%d' D: '%d'", ymd, ymd.Year, ymd.Month, ymd.Day)
-}
-
-func (ymd *YMD) Equals(other_ymd *YMD) bool {
-
-	if ymd.Year != other_ymd.Year {
-		return false
-	}
-
-	if ymd.Month != other_ymd.Month {
-		return false
-	}
-
-	if ymd.Day != other_ymd.Day {
-		return false
-	}
-
-	return true
-}
-
 /*
 
 Eventually this should be generated from the components pieces
@@ -140,63 +56,4 @@ do for now (20201223/thisisaaronland)
 
 func (d *EDTFDate) String() string {
 	return d.EDTF
-}
-
-type Timestamp struct {
-	timestamp int64
-}
-
-func NewTimestampWithTime(t *time.Time) *Timestamp {
-	return &Timestamp{t.Unix()}
-}
-
-func (ts *Timestamp) Time() *time.Time {
-
-	t := time.Unix(ts.Unix(), 0)
-	t = t.UTC()
-
-	return &t
-}
-
-func (ts *Timestamp) Unix() int64 {
-	return ts.timestamp
-}
-
-func (ts *Timestamp) UnmarshalJSON(b []byte) error {
-
-	s := strings.Trim(string(b), `"`)
-	i, err := strconv.ParseInt(s, 10, 64)
-
-	if err != nil {
-		return err
-	}
-
-	*ts = Timestamp{i}
-	return nil
-}
-
-func (ts Timestamp) MarshalJSON() ([]byte, error) {
-	str_ts := strconv.FormatInt(ts.timestamp, 10)
-	return []byte(str_ts), nil
-}
-
-// https://stackoverflow.com/questions/48050522/using-bitsets-in-golang-to-represent-capabilities
-
-type Precision uint32
-
-func (f Precision) HasFlag(flag Precision) bool { return f&flag != 0 }
-func (f *Precision) AddFlag(flag Precision)     { *f |= flag }
-func (f *Precision) ClearFlag(flag Precision)   { *f &= ^flag }
-func (f *Precision) ToggleFlag(flag Precision)  { *f ^= flag }
-
-func (f *Precision) IsAnnual() bool {
-	return f.HasFlag(YEAR)
-}
-
-func (f *Precision) IsMonthly() bool {
-	return f.HasFlag(MONTH)
-}
-
-func (f *Precision) IsDaily() bool {
-	return f.HasFlag(DAY)
 }
---
+++ edtf_test.go
@@ -0,0 +1,100 @@
+package edtf
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestUnmarshalJSON(t *testing.T) {
+
+	edtf_json := `{
+    "start": {
+      "edtf": "-1990",
+      "lower": {
+        "datetime": "-1990-01-01T00:00:00Z",
+        "timestamp": -124965504000,
+        "ymd": {
+          "year": -1990,
+          "month": 1,
+          "day": 1
+        },
+        "precision": 64
+      },
+      "upper": {
+        "datetime": "-1990-12-31T23:59:59Z",
+        "timestamp": -124933968001,
+        "ymd": {
+          "year": -1990,
+          "month": 12,
+          "day": 31
+        },
+        "precision": 64
+      }
+    },
+    "end": {
+      "edtf": "0400",
+      "lower": {
+        "datetime": "0400-01-01T00:00:00Z",
+        "timestamp": -49544438400,
+        "ymd": {
+          "year": 400,
+          "month": 1,
+          "day": 1
+        },
+        "precision": 64
+      },
+      "upper": {
+        "datetime": "0400-12-31T23:59:59Z",
+        "timestamp": -49512816001,
+        "ymd": {
+          "year": 400,
+          "month": 12,
+          "day": 31
+        },
+        "precision": 64
+      }
+    },
+    "edtf": "-1990/0400",
+    "level": 0,
+    "feature": "Time Interval"
+  }`
+
+	var d *EDTFDate
+
+	err := json.Unmarshal([]byte(edtf_json), &d)
+
+	if err != nil {
+		t.Logf("Failed to unmarshal JSON, %v", err)
+		t.Fail()
+		return
+	}
+
+	lower := d.Lower()
+
+	if lower == nil {
+		t.Logf("Expected lower time.Time but got none")
+		t.Fail()
+		return
+	}
+
+	upper := d.Upper()
+
+	if lower == nil {
+		t.Logf("Expected upper time.Time but got none")
+		t.Fail()
+		return
+	}
+
+	if lower.Unix() != -124965504000 {
+		t.Logf("Unexpected lower time.Time Unix timestamp")
+		t.Fail()
+		return
+	}
+
+	if upper.Unix() != -49512816001 {
+		t.Logf("Unexpected lower time.Time Unix timestamp")
+		t.Fail()
+		return
+	}
+
+}
