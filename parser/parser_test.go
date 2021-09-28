89fd23ee3cc42a85e584bce8625e2e09c3a3f842
thisisaaronland <aaron@localhost>
2021-09-28 13:54:31 -0700

    add IsOpen, IsUnspecified and IsUnknown methods to parser

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -157,3 +157,85 @@ func TestParseString(t *testing.T) {
 	}
 
 }
+
+func TestIsOpen(t *testing.T) {
+
+	is_open := []string{
+		edtf.OPEN,
+		edtf.OPEN_2012,
+	}
+
+	not_open := []string{
+		"",
+		"2021-09-28",
+	}
+
+	for _, s := range is_open {
+
+		if !IsOpen(s) {
+			t.Fatalf("String '%s' is considered open but reported as not open", s)
+		}
+	}
+
+	for _, s := range not_open {
+
+		if IsOpen(s) {
+			t.Fatalf("String '%s' is not open but reported as being open", s)
+		}
+	}
+
+}
+
+func TestIsUnspecified(t *testing.T) {
+
+	is_unspecified := []string{
+		edtf.UNSPECIFIED,
+		edtf.UNSPECIFIED_2012,
+	}
+
+	not_unspecified := []string{
+		"2021-09-28",
+	}
+
+	for _, s := range is_unspecified {
+
+		if !IsUnspecified(s) {
+			t.Fatalf("String '%s' is considered unspecified but reported as not unspecified", s)
+		}
+	}
+
+	for _, s := range not_unspecified {
+
+		if IsUnspecified(s) {
+			t.Fatalf("String '%s' is not unspecified but reported as being unspecified", s)
+		}
+	}
+
+}
+
+func TestIsUnknown(t *testing.T) {
+
+	is_unknown := []string{
+		edtf.UNKNOWN,
+		edtf.UNKNOWN_2012,
+	}
+
+	not_unknown := []string{
+		"2021-09-28",
+	}
+
+	for _, s := range is_unknown {
+
+		if !IsUnknown(s) {
+			t.Fatalf("String '%s' is considered unknown but reported as not unknown", s)
+		}
+	}
+
+	for _, s := range not_unknown {
+
+		if IsUnknown(s) {
+			t.Fatalf("String '%s' is not unknown but reported as being unknown", s)
+		}
+	}
+
+}
